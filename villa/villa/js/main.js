f08ade6573a391e257235f9f31d7c9d00c2c0caa
Thitikorn Padungwatsawat <60070018@kmitl.ac.th>
2018-04-25 02:34:16 +0700

    Edited Content
    - Fixed Navbar
    - Edited main contain
    - Change background
    - Added scrolldown-button
    - Delete recomment place in main
    etc.

--- villa/villa/js/main.js
+++ villa/villa/js/main.js
@@ -164,6 +164,13 @@
 	};
 	contentWayPoint();
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
 
 
 })(jQuery);
